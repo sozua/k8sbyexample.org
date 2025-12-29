cb40bbc8dbff037f69f343ff9940c06dbabd2c0d
Diogo de Souza <diogo@sozua.dev>
2025-12-29 12:18:35 -0300

    feat: add layout and ui components

---
+++ src/components/codeblock/index.ts
@@ -0,0 +1,4 @@
+export { default as Intro } from './Intro.astro';
+export { default as FileHeader } from './FileHeader.astro';
+export { default as TerminalHeader } from './TerminalHeader.astro';
+export { default as Row } from './Row.astro';
